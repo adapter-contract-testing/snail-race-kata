4907144ff8577df7a1a2c61765e503ea56ab9f5e
mathieu cans <mathieu.cans@orange.com>
2025-04-09 15:22:25 +0200

    add starting point for step2 in java

---
+++ snail-race-bet/java/src/test/java/snail/race/kata/adapters/RaceResultProviderHttpTest.java
@@ -0,0 +1,11 @@
+package snail.race.kata.adapters;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RaceResultProviderHttpTest {
+    @Test
+    void provide_something() {
+        Assertions.fail("Explore the endpoint with a unit test");
+    }
+}
