26886eb9bca39585c4cf8414c0f015f4b317eae5
mathieu cans <mathieu.cans@orange.com>
2025-04-16 22:30:38 +0200

    add race_result_provider_simulator

--- snail-race-bet/python/test/adapters/test_race_result_provider_http.py
+++ snail-race-bet/python/test/adapters/test_race_result_provider_http.py
@@ -1,8 +1,16 @@
 from src.adapters.race_result_provider_http import RaceResultProviderHttp
+from src.adapters.race_result_provider_http_internal_api import RaceResultProviderHttpInternalApi
 from test.domain.race_result_provider_contract import RaceResultProviderContract
 
 
 class TestRaceResultProviderHttp(RaceResultProviderContract):
+
+    def test_provide_something(self):
+        provider = self.create_race_result_provider()
+        results = provider.invoke_result_end_point()
+
+        assert isinstance(results, RaceResultProviderHttpInternalApi.RacesResponse)
+        assert len(results.races) > 0
 
     def create_race_result_provider(self):
         provider = RaceResultProviderHttp()
--- snail-race-bet/python/test/domain/race_result_provider_contract.py
+++ snail-race-bet/python/test/domain/race_result_provider_contract.py
@@ -9,13 +9,6 @@ class RaceResultProviderContract(ABC):
     def create_race_result_provider(self):
         pass
 
-    def test_provide_something(self):
-        provider = self.create_race_result_provider()
-        results = provider.invoke_result_end_point()
-
-        assert isinstance(results, RaceResultProviderHttpInternalApi.RacesResponse)
-        assert len(results.races) > 0
-
     def test_provide_race_result(self):
         provider = self.create_race_result_provider()
 
