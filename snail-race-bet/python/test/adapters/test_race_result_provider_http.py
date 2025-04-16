f1b3c5fd6473a4ac80b782dc9115d172a2da7ca1
mathieu cans <mathieu.cans@orange.com>
2025-04-16 22:22:28 +0200

    extract race result provider contract

--- snail-race-bet/python/test/adapters/test_race_result_provider_http.py
+++ snail-race-bet/python/test/adapters/test_race_result_provider_http.py
@@ -1,24 +1,10 @@
-import requests
-
 from src.adapters.race_result_provider_http import RaceResultProviderHttp
-from src.adapters.race_result_provider_http_internal_api import RaceResultProviderHttpInternalApi
-from src.domain.race_result_provider import SnailRaces
+from test.domain.race_result_provider_contract import RaceResultProviderContract
 
 
-class TestRaceResultProviderHttp:
+class TestRaceResultProviderHttp(RaceResultProviderContract):
 
-    def test_provide_something(self):
+    def create_race_result_provider(self):
         provider = RaceResultProviderHttp()
-        results = provider.invoke_result_end_point()
-
-        assert isinstance(results, RaceResultProviderHttpInternalApi.RacesResponse)
-        assert len(results.races) > 0
-
-    def test_provide_race_result(self):
-        provider = RaceResultProviderHttp()
-
-        races_result = provider.races()
-
-        assert isinstance(races_result, SnailRaces)
-        assert len(races_result.races) > 0
+        return provider
 
---
+++ snail-race-bet/python/test/domain/race_result_provider_contract.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+from src.adapters.race_result_provider_http_internal_api import RaceResultProviderHttpInternalApi
+from src.domain.race_result_provider import SnailRaces
+
+
+class RaceResultProviderContract(ABC):
+    @abstractmethod
+    def create_race_result_provider(self):
+        pass
+
+    def test_provide_something(self):
+        provider = self.create_race_result_provider()
+        results = provider.invoke_result_end_point()
+
+        assert isinstance(results, RaceResultProviderHttpInternalApi.RacesResponse)
+        assert len(results.races) > 0
+
+    def test_provide_race_result(self):
+        provider = self.create_race_result_provider()
+
+        races_result = provider.races()
+
+        assert isinstance(races_result, SnailRaces)
+        assert len(races_result.races) > 0
