33e8dd4c17b1731dacf12e6d6a0f9551bca22f01
mathieu cans <mathieu.cans@orange.com>
2025-04-17 17:32:40 +0200

    [python] Step1 done

---
+++ snail-race-bet/python/test/domain/test_bet_repository_simulator.py
@@ -0,0 +1,7 @@
+from snail_race_kata.domain.bet_repository_simulator import BetRepositorySimulator
+from test.domain.bet_repository_contract import BetRepositoryContract
+
+
+class TestBetRepositorySimulator(BetRepositoryContract):
+    def get_repository(self):
+        return BetRepositorySimulator()
