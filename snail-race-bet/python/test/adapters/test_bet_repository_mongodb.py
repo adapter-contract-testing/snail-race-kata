6cb42785e13dfa26a11474c55e2ef6ccb14b4424
mathieu cans <mathieu.cans@orange.com>
2025-04-17 17:26:33 +0200

    [python] Step1.1 ready for python

--- snail-race-bet/python/test/adapters/test_bet_repository_mongodb.py
+++ snail-race-bet/python/test/adapters/test_bet_repository_mongodb.py
@@ -1,7 +1,11 @@
+from copy import deepcopy
+
 import pytest
 from pymongo import MongoClient
 
 from snail_race_kata.adapters.bet_repository_mongodb import BetRepositoryMongoDb
+from snail_race_kata.domain.bet import Bet
+from snail_race_kata.domain.podium_pronostic import PodiumPronostic
 
 
 class TestBetRepositoryMongoDb():
@@ -15,12 +19,32 @@ class TestBetRepositoryMongoDb():
         yield
         self.client.close()
 
-    def test_register_a_bets(self):
-        # register a bet
-        # How can we verify the bet is actually inserted ?
-        # "Rien n'est plus dangereux qu'une idée quand on en a qu'une" (Emile Chartier)
-        # TIPS : Find some (>=3) options before commiting to one
-        assert False, "TODO : write the test, then the implementation😉"
+    def test_register_a_bet(self):
+        self.repository.register(Bet("Mathieu", PodiumPronostic(20, 12, 4), 12345))
+
+        bets = self.repository.find_by_date_range(12345, 12346)
+        assert len(bets) == 1
+        assert bets[0] == Bet("Mathieu", PodiumPronostic(20, 12, 4), 12345)
 
     def test_retrieve_only_bets_inside_the_time_range(self):
-        assert False , "TODO : write the test, then the implementation😉"
+        from_timestamp = 12346
+        to_timestamp = 12370
+
+        _bet_before_from = self.register_bet_at_timestamp(
+            self.repository, from_timestamp - 1
+        )
+        bet_on_from = self.register_bet_at_timestamp(self.repository, from_timestamp)
+        bet_after_from = self.register_bet_at_timestamp(self.repository, from_timestamp + 1)
+        bet_before_to = self.register_bet_at_timestamp(self.repository, to_timestamp - 1)
+        _bet_on_to = self.register_bet_at_timestamp(self.repository, to_timestamp)
+        _bet_after_to = self.register_bet_at_timestamp(self.repository, to_timestamp + 1)
+
+        bets = self.repository.find_by_date_range(from_timestamp, to_timestamp)
+
+        assert bets == [bet_on_from, bet_after_from, bet_before_to]
+
+
+    def register_bet_at_timestamp(self, repository, timestamp):
+        bet = Bet("Mathieu", PodiumPronostic(20, 12, 4), timestamp)
+        repository.register(bet)
+        return deepcopy(bet)
