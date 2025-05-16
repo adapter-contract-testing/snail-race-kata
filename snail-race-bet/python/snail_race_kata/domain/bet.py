37e2d6fddb4252ebc7ecb558e987dd7fd6c0d42d
mathieu cans <mathieu.cans@orange.com>
2025-05-16 16:17:14 +0200

    remove non domain methods

--- snail-race-bet/python/snail_race_kata/domain/bet.py
+++ snail-race-bet/python/snail_race_kata/domain/bet.py
@@ -8,13 +8,3 @@ class Bet:
     gambler: str
     pronostic: PodiumPronostic
     timestamp: int
-
-    def is_in_time_for(self, race):
-        return self.timestamp > race.timestamp - 2
-
-    def bet_is_on(self, podium):
-        return (
-            self.pronostic.first == podium.first.number
-            and self.pronostic.second == podium.second.number
-            and self.pronostic.third == podium.third.number
-        )
