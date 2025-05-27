c1ce52503a79ecdf749ed2477f00a1600eedadca
mathieu cans <mathieu.cans@orange.com>
2025-05-27 13:47:54 +0200

    Improve time constrain readabiliy for application test

--- snail-race-bet/java/src/test/java/snail/race/kata/domain/BetApplicationTest.java
+++ snail-race-bet/java/src/test/java/snail/race/kata/domain/BetApplicationTest.java
@@ -1,9 +1,10 @@
 package snail.race.kata.domain;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BetApplicationTest {
 
@@ -27,28 +28,23 @@ class BetApplicationTest {
     class WinOnlyWhenPronosticExactMatchsPodium {
         @Test
         void exact_match() {
-            betApplication.placeBet("me", 1, 9, 8, 7);
-            raceResultProvider.simulateRaceResult(33, 4, NINE_EIGHT_SEVEN_PODIUM);
+            betApplication.placeBet("me", betTime, 9, 8, 7);
+            raceResultProvider.simulateRaceResult(33, betTime + THREE_SECONDS, NINE_EIGHT_SEVEN_PODIUM);
             assertThat(betApplication.getWinnersForLastRace()).containsExactly(new Winner("me"));
         }
 
         @Test
         void third_place_differs() {
-            betApplication.placeBet("me", 1, 9, 8, 7);
-            raceResultProvider.simulateRaceResult(33, 5, new RaceResultProvider.Podium(
-                    new RaceResultProvider.Snail(9, "Not nine"),
-                    new RaceResultProvider.Snail(8, "Flash"),
-                    new RaceResultProvider.Snail(4, "Speedy")
-            ));
+            betApplication.placeBet("me", betTime, 9, 8, 7);
+            raceResultProvider.simulateRaceResult(33, betTime + THREE_SECONDS, NINE_HEIGHT_FOUR_PODIUM);
             assertThat(betApplication.getWinnersForLastRace()).isEmpty();
         }
     }
 
-    // test that bets placed less than 3 seconds before the race are not taken into account
     @Test
     void no_winner_when_bet_is_placed_less_than_3_seconds() {
-        betApplication.placeBet("me", 1, 9, 8, 7);
-        raceResultProvider.simulateRaceResult(2, 3, NINE_EIGHT_SEVEN_PODIUM);
+        betApplication.placeBet("me", betTime, 9, 8, 7);
+        raceResultProvider.simulateRaceResult(2, betTime + TWO_SECONDS, NINE_EIGHT_SEVEN_PODIUM);
         assertThat(betApplication.getWinnersForLastRace()).isEmpty();
     }
 
@@ -67,22 +63,26 @@ class BetApplicationTest {
         assertThat(betApplication.getWinnersForLastRace()).isEmpty();
     }
 
-    @Disabled
-    void no_winners_when_there_is_the_slightest_difference() {
-//        betApplication.placeBet("me", 1, 9
-    }
-
     public static final RaceResultProvider.Podium NINE_EIGHT_SEVEN_PODIUM = new RaceResultProvider.Podium(
             new RaceResultProvider.Snail(9, "Turbo"),
             new RaceResultProvider.Snail(8, "Flash"),
             new RaceResultProvider.Snail(7, "Speedy")
     );
+    public static final RaceResultProvider.Podium NINE_HEIGHT_FOUR_PODIUM = new RaceResultProvider.Podium(
+            new RaceResultProvider.Snail(9, "Not nine"),
+            new RaceResultProvider.Snail(8, "Flash"),
+            new RaceResultProvider.Snail(4, "Speedy")
+    );
+
     public static final RaceResultProvider.Podium FIVE_SIX_SEVEN_PODIUM = new RaceResultProvider.Podium(
             new RaceResultProvider.Snail(5, "Gonzales"),
             new RaceResultProvider.Snail(6, "Gordon"),
             new RaceResultProvider.Snail(7, "Speedy")
     );
 
+    public final int TWO_SECONDS = 2;
+    public final int THREE_SECONDS = 3;
     public final int FIVE_SECONDS = 5;
     public final int TEN_SECONDS = 10;
+    public final int betTime = 123456;
 }
